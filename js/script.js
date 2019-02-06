4a182ffe3c8689965db066fdd06870a65feced85
Brett Kays <bkschoolmail@gmail.com>
2019-02-05 23:21:03 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$( document ).ready(() => {
+
+	/* When minus is clicked, decrease life total by one */
+	$('.left').click(function() {
+		var lifeTotal = $( this ).siblings( '.life-total' ).text()
+		
+		lifeTotal = lifeTotal - 1
+
+		$( this ).siblings( '.life-total' ).text( lifeTotal )
+	})
+
+	$('.right').click(function() {
+		var lifeTotal = $( this ).siblings( '.life-total' ).text()
+
+		lifeTotal = parseInt( lifeTotal )
+		lifeTotal = lifeTotal + 1
+
+		$( this ).siblings( '.life-total' ).text( lifeTotal )
+	})
+	
+	$('#refresh-button').click(function() {
+		$( '.life-total' ).text( '20' )
+	})
+
+	$( '#die-roll_button' ).click(function() {
+  		$( '.die-roll' ).toggle()
+  	
+  		$( '.die-roll__number' ).each(function() {
+  			var value = Math.floor(Math.random() * 6) + 1
+
+  			$( this ).text( value )
+  		})
+
+  	})
+})
